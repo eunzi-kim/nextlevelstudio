f5707deeb6f30a0ec75fbe992b6015ecb0bd0af8
eunzi-kim <rladmswlek@naver.com>
2021-12-28 11:56:13 +0900

    initial setting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './Components/Login';
+import Main from './Components/Main';
+import Detail from './Components/Detail';
+
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>      
+      <Routes>
+        <Route path="/sign-in" element={<Login />} />
+        <Route path="/" element={<Main />} />
+        <Route path="/content/:id" element={<Detail />} />     
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
